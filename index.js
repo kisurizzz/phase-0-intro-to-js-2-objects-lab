1cfc3a4fda9f900a0c7b6f13bca8ea38fe558010
Arnold Kisuri <arnoldkisuri7@gmail.com>
2024-03-22 13:19:25 +0300

    Initial Commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Kisuri',
+    streetAddress: '11 Broadway'
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee, [key]: value}
+    return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+}
+
+deleteFromEmployeeByKey = (employee, key, value) => {
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+destructivelyDeleteFromEmployeeByKey = (employee, key, value) => {
+    delete employee[key]
+    return employee
+}
